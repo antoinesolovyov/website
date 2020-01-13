416f35560198f5a9efb831c90b1bd0794d805251
antoinesolovyov <antoinesolovyov@gmail.com>
2020-01-13 11:11:17 +0300

    Just commit

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -4,15 +4,26 @@ import "./Article.css";
 
 const Article = () => {
     return (
-        <article>
-            <h3>Neuron</h3>
-            <p>
-                A neuron (also called neurone or nerve cell) is a cell that
-                carries electrical impulses. Neurons are the basic (functional and
-                structural) units of our nervous system.
-            </p>
-            <img src="imgs/neuron.png" alt="neuron" />
-        </article>
+        <>
+            <article>
+                <h3>Neuron</h3>
+                <p>
+                    A neuron (also called neurone or nerve cell) is a cell that
+                    carries electrical impulses. Neurons are the basic
+                    (functional and structural) units of our nervous system.
+                </p>
+                <img src="imgs/neuron.png" alt="neuron" />
+            </article>
+            <article>
+                <h3>Artificial neural networks</h3>
+                <p>
+                    Artificial neural networks (ANN) or connectionist systems
+                    are computing systems vaguely inspired by the biological
+                    neural networks that constitute animal brains.
+                </p>
+                <img src="imgs/NeuralNetwork.png" alt="neuron" />
+            </article>
+        </>
     );
 };
 
